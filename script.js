66f651de295fe48a8657381d85f07f0f8e0ccd4a
mmsanchezr <106345843+mmsanchezr@users.noreply.github.com>
2022-06-07 22:05:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function validatePrincipal()
 
 function compute() {
 
-    validatePrincipal();
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
