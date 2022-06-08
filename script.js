f640ddbfee83595f9883fe3f79073c18e05ef7ac
Monica Sanchez <mmsanchezramirez@gmail.com>
2022-06-08 02:11:12 +0000

    fix javascript 3

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest=principal * years * rate / 100
-var year = new Date().getFullYear()+parseInt(years);
-
-
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-
-
-}
-       
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
+}
+
+function compute() {
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100
+    var year = new Date().getFullYear()+parseInt(years);
+
+
+    document.getElementById("result").innerHTML= 
+    "If you deposit <mark>" + principal  + "</mark>" +
+    "<br>at an interest of <mark> " + rate + "</mark>" +
+    "<br>You will receive an amount of <mark> "+ amount + "</mark>" +
+    "<br> in the year <mark> " + year + "</mark>";
+
+
 }
