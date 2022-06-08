a820e2303afe7151b3aac188c4a0f0b18164ca8c
mmsanchezr <106345843+mmsanchezr@users.noreply.github.com>
2022-06-07 22:03:37 -0500

    Update script.js
    validation

--- script.js
+++ script.js
@@ -25,12 +25,18 @@ function compute() {
     var amount = principal * years * rate / 100
     var year = new Date().getFullYear()+parseInt(years);
 
+    if (principal <= 0)
+    {
+        alert("Enter a Positive Number");
+        document.getElementById("principal").focus();
+    }
+    else {
 
     document.getElementById("result").innerHTML= 
     "If you deposit <mark>" + principal  + "</mark>" +
     "<br>at an interest of <mark> " + rate + "</mark>" +
     "<br>You will receive an amount of <mark> "+ amount + "</mark>" +
     "<br> in the year <mark> " + year + "</mark>";
-
+    }
 
 }
